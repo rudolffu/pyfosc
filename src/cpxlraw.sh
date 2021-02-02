00c7a87502ee8396596bfa1178946e6fb0f51b17
Yuming Fu <fuympku@outlook.com>
2021-02-02 16:12:42 +0800

    modified src/cpxlraw.sh

--- src/cpxlraw.sh
+++ src/cpxlraw.sh
@@ -5,10 +5,11 @@ do
 	cp "$log" "$logbak"
 done
 
-for name in 20*fit
+for name in 20*fit # new filename standards in 2021
 do
     # newname="$(echo "$name" | cut -c 11-)"
-    newname="${name:(-8)}"
+    newid="$(echo "$name" | cut -c 9-12)" 
+    newname="${newid}.fit"
     cp "$name" "$newname"
 done
 if [ ! -d "./raw" ]; then
