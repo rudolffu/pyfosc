fdc2e06b1533a32b43bef1b60b2259d996052617
rudolffu <fuympku@outlook.com>
2023-01-06 08:05:09 +0800

    update removecr_ccdp.py

--- src/removecr_ccdp.py
+++ src/removecr_ccdp.py
@@ -8,25 +8,31 @@ import platform
 import sys
 import json
 import shutil
+import glob
+import re
+from astropy.io import fits
+import numpy as np
 
 if version.parse(platform.python_version()) < version.parse("3.0.0"):
     print("Using raw_input")
 else:
     raw_input = input
 
-basepath = os.path.dirname(sys.argv[0])
-lacos_im = os.path.join(basepath, '../iraf_tasks/lacos_im.cl')
 with open('myfosc.json') as file:
     settings = json.loads(file.read())
 teles = settings['mysettings']['telescope']
 if teles == "XLT":
     print("Settings for XLT will be used.")
-    gain = 1.41
-    readnoise = 4.64
+    gain = 2.2
+    readnoise = 7.8
 elif teles == "LJT":
     print("Settings for LJT will be used.")
-    gain = 3.20
-    readnoise = 13.5
+    gain = 1.1
+    readnoise = 6.6
+elif teles == "HCT":
+    print("Settings for HCT will be used.")
+    gain = 1.22
+    readnoise = 4.8
 else:
     print("Error detected.")
 
@@ -42,8 +48,21 @@ files = ccdp.ImageFileCollection('./', keywords=keys,filenames=objall)
 co_targets = files.filter()
 
 # Process standard star and targets separately
-idx1 = (co_targets.summary['exptime'] > 500)
-tbex = co_targets.summary[co_targets.summary['exptime'] < 900]
+print('Spectra after flat field correction:\n' + ", ".join(p for p in objall))
+co_targets.summary
+stdspec = str(raw_input("Enter filename of the standard star spectra \
+(separated by ',' for more than 1): "))
+stdlist = re.split(',|,\s+', stdspec)
+stdexptime = []
+for stdspec in stdlist:
+    stdspec = stdspec.strip('.fits') + '.fits'
+    hdr = fits.getheader(stdspec)
+    exptime = float(hdr['EXPTIME'])
+    stdexptime.append(exptime)
+stdexptime = np.array(stdexptime)
+maxexptime = np.max(stdexptime)
+idx_tar = (co_targets.summary['exptime'] > maxexptime)
+tbex = co_targets.summary[co_targets.summary['exptime'] <= maxexptime]
 
 # For standard stars, done
 for i, pathname in enumerate(tbex['file']):
@@ -59,8 +78,14 @@ flat1 = CCDData.read('./perFlat.fits', unit='adu')
 fmask = ccdp.ccdmask(flat1)
 
 # For targets
-tb1 = co_targets.summary[idx1]
-for file_path in tb1['file'].tolist():
+tb_tar = co_targets.summary[idx_tar]
+try:
+    hdr = fits.getheader(tb_tar['file'].tolist()[0])
+    gain = hdr['GAIN']
+    readnoise = hdr['RDNOISE']
+except:
+    pass
+for file_path in tb_tar['file'].tolist():
     file_name = os.path.basename(file_path)
     ccd = CCDData.read(file_path, unit='adu')
     ccd = ccdp.gain_correct(ccd, gain * u.electron / u.adu)
