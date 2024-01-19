1a6dd3c22b6e40234ded1aefa64c914d18af6154
rudolffu <fuympku@outlook.com>
2024-01-19 16:25:29 +0800

    update script pyfosc_run.sh

--- pyfosc_run.sh
+++ pyfosc_run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SRCDIR=$BASEDIR/src
-python $SRCDIR/makezero2m.py
-python $SRCDIR/ccdotz.py
+python $SRCDIR/makezero_ccdp.py
+python $SRCDIR/ccdotz_ccdp.py
 python $SRCDIR/makeflat2m.py
 python $SRCDIR/makereflat2m.py
 python $SRCDIR/divideflat2m.py
